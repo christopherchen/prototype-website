c57a1c1838846904c3e791feea53a83be6d017fc
Christopher Chen <christopher.nh.chen@gmail.com>
2019-01-22 04:56:29 +0000

    Refactor app.js

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,33 +3,53 @@
 $(document).ready(function() {
   
   
-  $('#about').click(function(event){
-    event.preventDefault();
-    $('#about').addClass('active');
-    $('.about').removeClass('hidden');
-    $('#prototype').removeClass('active');
-    $('.prototype').addClass('hidden');
-    $('#contact').removeClass('active');
-    $('.contact').addClass('hidden');
-  });
+  // $('#about').click(function(event){
+  //   event.preventDefault();
+  //   $('#about').addClass('active');
+  //   $('.about').removeClass('hidden');
+  //   $('#prototype').removeClass('active');
+  //   $('.prototype').addClass('hidden');
+  //   $('#contact').removeClass('active');
+  //   $('.contact').addClass('hidden');
+  // });
   
-  $('#prototype').click(function(event){
-    event.preventDefault();
-    $('#prototype').addClass('active');
-    $('.prototype').removeClass('hidden');
-    $('#about').removeClass('active');
-    $('.about').addClass('hidden');
-    $('#contact').removeClass('active');
-    $('.contact').addClass('hidden');
-  });
+  // $('#prototype').click(function(event){
+  //   event.preventDefault();
+  //   $('#prototype').addClass('active');
+  //   $('.prototype').removeClass('hidden');
+  //   $('#about').removeClass('active');
+  //   $('.about').addClass('hidden');
+  //   $('#contact').removeClass('active');
+  //   $('.contact').addClass('hidden');
+  // });
   
-  $('#contact').click(function(event){
+  // $('#contact').click(function(event){
+  //   event.preventDefault();
+  //   $('#contact').addClass('active');
+  //   $('.contact').removeClass('hidden');
+  //   $('#about').removeClass('active');
+  //   $('.about').addClass('hidden');
+  //   $('#prototype').removeClass('active');
+  //   $('.prototype').addClass('hidden');
+  // })
+  
+  var tabs = ['contact', 'about', 'prototype'];
+  
+  function eventHandler(event){
     event.preventDefault();
-    $('#contact').addClass('active');
-    $('.contact').removeClass('hidden');
-    $('#about').removeClass('active');
-    $('.about').addClass('hidden');
-    $('#prototype').removeClass('active');
-    $('.prototype').addClass('hidden');
-  })
+    tabs.forEach(function(item){
+      if(event.currentTarget.id === item){
+        $('#' + item).addClass('active');
+        $('.' + item).removeClass('hidden');
+      } else {
+        $('#' + item).removeClass('active');
+        $('.' + item).addClass('hidden');
+      }
+    });
+  }
+  
+  $('#prototype').click(eventHandler);
+  $('#about').click(eventHandler);
+  $('#contact').click(eventHandler);
+
 });
